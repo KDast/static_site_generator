53dfdf520ac5ce17946d6657db8c543e56c879a8
KDast <daoustkevin07@gmail.com>
2024-12-21 12:46:02 -0500

    CH2 L1

---
+++ src/main.py
@@ -0,0 +1,21 @@
+from textnode import *
+
+def main():
+    object = TextNode("This is a test", TextType.BOLD, "https://www.boot.dev")
+    
+    
+    print(object)
+
+
+
+
+
+
+
+
+
+
+
+
+
+main()
---
+++ src/textnode.py
@@ -0,0 +1,28 @@
+from enum import Enum
+
+class TextType(Enum):
+    NORMAL = "normal"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINKS = "links"
+    IMAGES = "images"
+
+
+
+class TextNode():
+    def __init__(self, text, text_type, url):
+        self.text = text
+        self.text_type = text_type
+        if len(url) == 0:
+            return None
+        if type(url) == str:
+            self.url = url
+
+    def __eq__(self, object2):
+        return (
+            self.text == object2.text and self.text_type == object2.text_type and self.ulr == object2.url
+            )
+
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"       
